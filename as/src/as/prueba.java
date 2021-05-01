78a68e2c437676951b1ddbdb97b15feefe6b5953
ales <alexnaula57@gmail.com>
2021-04-30 23:18:33 -0500

    rqrqw

---
+++ as/src/as/prueba.java
@@ -0,0 +1,14 @@
+package as;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String al= "hola";
+		
+		System.out.println(al);
+
+	}
+
+}
